894eab2e5c6a08ddca0d2f9d30809cfbe4e817c8
Ryne Vitek <rjvitek@hotmail.com>
2015-12-07 22:33:28 +0000

    TDD Lesson 3

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Ryne Vitek', :saying => 'Gooble Gob, Gobble Goob!')
+		expected = 'RV#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
 end
