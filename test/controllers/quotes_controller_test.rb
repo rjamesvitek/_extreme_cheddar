b8145259406d5c0027df31ee1210521de39433e5
Ryne Vitek <rjvitek@hotmail.com>
2015-12-09 21:14:48 +0000

    TDD Final

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
 	test "quote show page" do
-    	quote = Quote.create(:author => 'Ryne Vitek', :saying => 'Gooble Gob, Gobble Goob!')
+    	quote = FactoryGirl.create(:quote)
     	get :show, :id => quote.id
     	assert_response :success
 	end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do
-		quote = Quote.create(:author => 'Ryne Vitek', :saying => 'Gooble Gob, Gobble Goob!')
+		quote = FactoryGirl.create(:quote, :author => 'Ryne Vitek')
 		expected = 'RV#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual
